8b32f25d0afebdb9c8577b1275abe5f71b7a8478
Yulia <yuliakir2011@gmail.com>
2016-01-09 07:41:00 +0300

    добавлено

---
+++ WebApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Управление общими сведениями о сборке осуществляется с помощью 
+' набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+' связанных с этой сборкой.
+
+' Проверка значений атрибутов сборки
+<Assembly: AssemblyTitle("WebApp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+<Assembly: Guid("63bd0b34-5436-4044-8d12-a7875494476e")>
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
